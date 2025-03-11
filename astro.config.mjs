23849ff4819b6f6311e8a87baf3585514a71eb34
yooeslie <eloise.marien@etu.unilim.fr>
2025-03-11 11:24:39 +0100

    test-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yooeslie.github.io',
+  base: 'test-TD',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
